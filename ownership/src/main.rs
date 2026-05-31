1c51910be1f6d3b85a1d845fcac91b33337d6dc2
= <=>
2026-05-31 23:53:41 +0530

    Add scope of the immutable references : ends where they are last used

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -77,16 +77,45 @@
 //
 // fn calculate_length(s: &String) -> usize {
 //     s.len()
+// // }
+//
+// fn main() {
+//     let mut s = String::from("hello");
+//
+//     change(&mut s);
+//
+//         let mut s = String::from("hello");
+//     //
+//     // {
+//     //     let r1 = &mut s;
+//     // } // r1 goes out of scope here, so we can make a new reference with no problems.
+//     //
+//     // let r2 = &mut s;
+//
+//     println!("Value of s is {s}");
+// }
+//
+// fn change(some_string: &mut String) {
+//     some_string.push_str(", world");
 // }
 
+// NOTE: BORROWING PREVENTS RACES CAUSED BY:
+// Two or more pointers access the same data at the same time.
+// At least one of the pointers is being used to write to the data.
+// There’s no mechanism being used to synchronize access to the data.
+
+//MUTABLE AND IMMMUTABLE REFERECENCES
+//scope
+
 fn main() {
+    //This code doesnt compile
     let mut s = String::from("hello");
 
-    change(&mut s);
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    // let r3 = &mut s; // BIG PROBLEM
+    println!("{r1} and {r2}");
 
-    println!("Value of s is {s}");
-}
-
-fn change(some_string: &mut String) {
-    some_string.push_str(", world");
+    let r3 = &mut s;
+    println!("{r3}");
 }
