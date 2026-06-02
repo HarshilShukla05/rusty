303a3cbb0034b288559854558169c5a3b5460829
= <=>
2026-06-02 22:05:22 +0530

    Ownershipe Complete : Slices

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -121,19 +121,19 @@
 // }
 
 //DANGLING REFERECENCES: By design never happens in rust
-fn main() {
-    let reference_to_nothing = dangle();
-
-    println!("{reference_to_nothing}");
-}
-
-fn dangle() -> String {
-    // let s = String::from("hello");
-    //
-    // &s
-    let s = String::from("thread_local!");
-    s
-} //DROP the reference of s , and the fnc is retrunings its reference only
+// fn main() {
+//     let reference_to_nothing = dangle();
+//
+//     println!("{reference_to_nothing}");
+// }
+//
+// fn dangle() -> String {
+//     // let s = String::from("hello");
+//     //
+//     // &s
+//     let s = String::from("thread_local!");
+//     s
+// } //DROP the reference of s , and the fnc is retrunings its reference only
 //Means the reference would be pointing to an invalid string
 
 // SOLUTION: Retunr the string directly
@@ -142,3 +142,53 @@ fn dangle() -> String {
 /*
  * At any given time, you can have either one mutable reference or any number of immutable references.
  * References must always be valid.*/
+
+/*------------------------------------SLICES--------------------------------------------------------*/
+//Here’s a small programming problem: Write a function that takes
+//a string of words separated by spaces and returns the first word
+//it finds in that string. If the function doesn’t find a space in the
+//string, the whole string must be one word, so the entire string should be returned.
+
+fn main() {
+    // let mut s = String::from("hello world");
+    //
+    // let word = first_word(&s); // word will get the value 5
+    //
+    // s.clear(); // this empties the String, making it equal to ""
+
+    // word still has the value 5 here, but s no longer has any content that we
+    // could meaningfully use with the value 5, so word is now totally invalid!
+
+    //HENCE SLICES--------------------------------------------------------
+    //
+    // let s = String::from("hello world");
+    //
+    // let _hello = &s[0..5];
+    // let _world = &s[6..11];
+    //
+    // let ans = first_word(&s);
+    //
+    // println!("Ans is {ans}");
+
+    //BUG:
+    // let mut s = String::from("hello world");
+    //
+    // let word = first_word(&s);
+    //
+    // // s.clear(); // error!
+    //
+    // println!("the first word is: {word}");
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            // return i; Return a SLICE
+            return &s[..i];
+        }
+    }
+
+    &s[..]
+}
